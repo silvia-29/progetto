ba1b89019398c2d928df757eeba17ed623557e65
MattiaCarnazzo <carnazzomattia0@gmail.com>
2023-10-16 11:58:50 +0200

    k

--- Progetto.js
+++ Progetto.js
@@ -20,6 +20,7 @@ console.log(quantitaNumeriPari); // Output: 4
 
 
 
+
 const arr2 = [1, 2, 3, 4, 5];
 const sommaArray = arr => arr.reduce((acc, numero) => acc + numero, 0);
 console.log(sommaArray(arr2));
